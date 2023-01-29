37b0fdba49bd9c50ebfa1fb1a225cdedc3d9d897
DC <capota.d@gmail.com>
2023-01-29 12:28:35 -0600

    (WIP) Rough attempt at 6DOF EOM

--- Assets/Scripts/ForceVector.cs
+++ Assets/Scripts/ForceVector.cs
@@ -6,20 +6,36 @@ public class ForceVector : MonoBehaviour
 {
     public Vector3 vec;
 
+
+    
+    public GameObject TailObject;
+    public GameObject TipObject;
+    public float magnitude = 1;
+
+
+    public Vector3 Tail;
+    public Vector3 Tip;
+    
+
     public ForceVector(float x, float y, float z)
     {
         vec = new Vector3(x, y, z);
+        magnitude = 1;
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        Tail = TailObject.transform.parent.localPosition;
+        Tip = Tail + TipObject.transform.localPosition;
+
+        vec = (Tip - Tail) * magnitude;
         Debug.Log("vec: " + vec);
     }
 }
--- Assets/Scripts/PhysicsEngine.cs
+++ Assets/Scripts/PhysicsEngine.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class PhysicsEngine : MonoBehaviour
 {
     public GameObject go;
+
+    public GameObject force1;
     
     // Start is called before the first frame update
     void Start()
@@ -20,7 +22,29 @@ public class PhysicsEngine : MonoBehaviour
     {
         f.vec = go.GetComponent<Transform>().position;
         Debug.Log("f.vec: " + f.vec);
+
+
+        // calculate velocity from force and move position accordingly
+        velocity += force1.GetComponent<ForceVector>().vec * Time.deltaTime;
+        velocity -= velocity * dragCoefficient;
+
+        this.transform.parent.position += velocity * Time.deltaTime;
+
+        // try to do the same for rotations
+        netTorque = Vector3.Cross(force1.GetComponent<ForceVector>().vec, force1.transform.position);
+        angularVelocity += netTorque * Time.deltaTime;
+        angularVelocity -= angularVelocity * dragCoefficient; // fictituous drag term
+        angularPositionDelta = angularVelocity * Time.deltaTime;
+        this.transform.parent.rotation *=  Quaternion.Euler(angularPositionDelta); // you have to multiply quaternions together to combine them
     }
+
+    public Vector3 velocity = new Vector3(0, 0, 0);
+
+    public Vector3 netTorque = new Vector3(0, 0, 0);
+    public Vector3 angularVelocity = new Vector3(0, 0, 0);
+    public Vector3 angularPositionDelta = new Vector3(0, 0, 0);
+
+    public float dragCoefficient = 0.002f;
 
     private ForceVector f;
 }
